e3fbd99a478d00f122d9eb2df335af75cf2439bf
Geronimogomez09 <geronimogomezepet20@gmail.com>
2026-01-21 20:10:08 -0300

    agregando mas contenido y estructura base

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const logo = document.getElementById("logo");
+const intro = document.getElementById("intro");
 const contenido = document.getElementById("contenido");
+const body = document.body;
 
 logo.addEventListener("click", () => {
-    contenido.classList.toggle("activo");
+    intro.classList.add("oculto");
+
+    setTimeout(() => {
+        intro.style.display = "none";   // ahora sí desaparece
+        body.classList.add("fondo2");
+        contenido.style.display = "grid";
+        contenido.classList.add("activo");
+    }, 800);
 });
+const fecha = document.getElementById("fecha");
+
+const hoy = new Date();
+const opciones = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+};
+
+fecha.textContent = hoy.toLocaleDateString("es-AR", opciones);
